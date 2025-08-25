ad8bc1a3bc3db4229493eef233955a84ec36a505
kovalevv <vl.kovalevskyi@gmail.com>
2025-08-26 01:13:20 +0200

    Solution2 with fixes

--- app/main.py
+++ app/main.py
@@ -1,21 +1,33 @@
-class Animal:
-    alive = []
+from __future__ import annotations
 
-    def __init__(self,
-                 name: str,
-                 health: int = 100,
-                 hidden: bool = False) -> None:
+
+class AliveList(list):
+    def __repr__(self) -> str:
+        return str([
+            {
+                "Name": animal.name,
+                "Health": animal.health,
+                "Hidden": animal.hidden,
+            }
+            for animal in self
+        ])
+
+
+class Animal:
+    alive: AliveList = AliveList()
+
+    def __init__(self, name: str, health: int = 100) -> None:
         self.name = name
         self.health = health
-        self.hidden = hidden
-        if self.health > 0:
-            Animal.alive.append(self)
+        self.hidden: bool = False
+        Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return "{" + \
-            (f"Name: {self.name}, Health: {self.health}, ") + \
-            (f"Hidden: {self.hidden}") \
-            + "}"
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
 
 
 class Herbivore(Animal):
@@ -24,9 +36,15 @@ class Herbivore(Animal):
 
 
 class Carnivore(Animal):
-    def bite(self, another_animal: Herbivore) -> None:
-        if (isinstance(another_animal, Herbivore)
-                and another_animal.hidden is not True):
-            another_animal.health -= 50
-            if another_animal.health <= 0:
-                Animal.alive.remove(another_animal)
+    def bite(self, herbivore: Herbivore) -> None:
+
+        if isinstance(herbivore, Carnivore):
+            return
+
+        if herbivore.hidden:
+            return
+
+        herbivore.health -= 50
+        if herbivore.health <= 0:
+            herbivore.health = 0
+            Animal.alive.remove(herbivore)
